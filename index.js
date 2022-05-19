83d2e2eea381d0a5effd374e562a0fec28416d94
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-05-19 06:20:41 +0600

    finished

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
 const app = express();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@kiron.ripcl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -21,9 +37,17 @@ async function run() {
     await client.connect();
     const taskCollection = client.db("taskCollection").collection("tasks");
 
-    app.get("/tasks", async (req, res) => {
+    app.get("/tasks", verifyJWT, async (req, res) => {
       const tasks = await taskCollection.find({}).toArray();
       res.send(tasks);
+    });
+
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
     });
 
     // task post
@@ -38,6 +62,30 @@ async function run() {
       const id = req.params.id;
       const result = await taskCollection.deleteOne({ _id: ObjectId(id) });
       res.send(result);
+    });
+    app.patch("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: { completed: true },
+      };
+      const result = await taskCollection.updateOne(filter, updateDoc);
+      if (result.acknowledged) {
+        res.send({ success: true, message: "Task completed" });
+      } else {
+        res.send({ success: false, message: "Task not completed" });
+      }
+    });
+
+    app.get("/my-tasks", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const myItems = await taskCollection.find({ email: email }).toArray();
+        res.send(myItems);
+      } else {
+        res.status(403).send({ message: "forbidden access" });
+      }
     });
   } finally {
   }
